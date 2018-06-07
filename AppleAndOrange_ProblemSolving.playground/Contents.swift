87486fedcbebea2c0edbee334375fbdcd5b318d9
Magdalena <goferek23@gmail.com>
2018-06-07 23:48:28 +0200

    add Apple and Orange, solved ArrayDS

---
+++ AppleAndOrange_ProblemSolving.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let s = 7, t = 11 // starting & ending point of Sam's house
+let a = 5, b = 15 // location of apple & orange tree
+let m = 3, n = 2 // num of apples & oranges tat fell from the tree
+let oA = [-2, 2, 1] // distances of each apple from point a
+let oD = [5, -6] // distances of each apple from point b
+var aFallen = 0, oFallen = 0 // apples & oranges that fall on house
+
+var apples: [Int] = [] // distance at which each apple falls from tree
+var oranges: [Int] = [] // distance at which each orange falls from tree
+
+func countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: [Int], oranges: [Int]) -> Void {
+    
+    
+    print(aFallen, oFallen)
+}
+
+countApplesAndOranges(s: s, t: t, a: a, b: b, apples: apples, oranges: oranges)
