1449239c61b6824bc35e26db692f315f97a913b1
Magdalena <goferek23@gmail.com>
2018-06-08 07:18:01 +0200

    solved apple and orange

--- AppleAndOrange_ProblemSolving.playground/Contents.swift
+++ AppleAndOrange_ProblemSolving.playground/Contents.swift
@@ -1,19 +1,64 @@
 import Foundation
 
-let s = 7, t = 11 // starting & ending point of Sam's house
-let a = 5, b = 15 // location of apple & orange tree
-let m = 3, n = 2 // num of apples & oranges tat fell from the tree
-let oA = [-2, 2, 1] // distances of each apple from point a
-let oD = [5, -6] // distances of each apple from point b
-var aFallen = 0, oFallen = 0 // apples & oranges that fall on house
-
-var apples: [Int] = [] // distance at which each apple falls from tree
-var oranges: [Int] = [] // distance at which each orange falls from tree
-
+// Complete the countApplesAndOranges function below.
 func countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: [Int], oranges: [Int]) -> Void {
+    var aFallen = 0, oFallen = 0 // apples & oranges that fall on house
+    var dApples: [Int] = [] // distance at which each apple falls from tree
+    var dOranges: [Int] = [] // distance at which each orange falls from tree
+    dApples += apples.map{a+$0}
+    dOranges += oranges.map{b+$0}
     
-    
-    print(aFallen, oFallen)
+    let applesInHouseRange = dApples.filter{s...t ~= $0}
+    let orangesInHouseRange = dOranges.filter{s...t ~= $0}
+    aFallen = applesInHouseRange.count
+    oFallen = orangesInHouseRange.count
+    print(aFallen)
+    print(oFallen)
 }
+
+let stTemp = "7 11"
+let st = stTemp.split(separator: " ").map{ String($0) }
+
+guard let s = Int(st[0].trimmingCharacters(in: .whitespacesAndNewlines))
+    else { fatalError("Bad input") }
+
+guard let t = Int(st[1].trimmingCharacters(in: .whitespacesAndNewlines))
+    else { fatalError("Bad input") }
+
+let abTemp = "5 15"
+let ab = abTemp.split(separator: " ").map{ String($0) }
+
+guard let a = Int(ab[0].trimmingCharacters(in: .whitespacesAndNewlines))
+    else { fatalError("Bad input") }
+
+guard let b = Int(ab[1].trimmingCharacters(in: .whitespacesAndNewlines))
+    else { fatalError("Bad input") }
+
+let mnTemp = "3 2"
+let mn = mnTemp.split(separator: " ").map{ String($0) }
+
+guard let m = Int(mn[0].trimmingCharacters(in: .whitespacesAndNewlines))
+    else { fatalError("Bad input") }
+
+guard let n = Int(mn[1].trimmingCharacters(in: .whitespacesAndNewlines))
+    else { fatalError("Bad input") }
+
+let applesTemp = "-2 2 1"
+let apples: [Int] = applesTemp.split(separator: " ").map {
+    if let applesItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return applesItem
+    } else { fatalError("Bad input") }
+}
+
+guard apples.count == m else { fatalError("Bad input") }
+
+let orangesTemp = "5 -6"
+let oranges: [Int] = orangesTemp.split(separator: " ").map {
+    if let orangesItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return orangesItem
+    } else { fatalError("Bad input") }
+}
+
+guard oranges.count == n else { fatalError("Bad input") }
 
 countApplesAndOranges(s: s, t: t, a: a, b: b, apples: apples, oranges: oranges)
